2e2f90330b23e1403cfddf9955ec6de0dc0bbc28
alexpixel41 <nirk.maroto@gmail.com>
2017-11-19 14:11:42 +0100

    fixed errors

---
+++ src/AppBundle/Form/CursoType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class CursoType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('titulo',TextType::class,array("required"=>"required","attr"=>array(
+            "class"=>"form-titulo"
+            )))
+        ->add('descripcion',TextareaType::class)
+        
+        // # select 
+        // ->add('precio', ChoiceType::class,array(
+        //     "choices"=>array(
+        //     "hombre"=>"Hombre",
+        //     "mujer"=>"Mujer",
+        //     "cosa"=>"Cosa"
+        //     )))
+
+        // # checkbox 
+        // ->add('precio',CheckboxType::class,array(
+        //     "label"=>"Mostrar precio?",
+        //     "required"=>true
+        // ))
+
+        ->add('precio',TextType::class)
+        ->add('submit',SubmitType::class);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Curso'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_curso';
+    }
+
+
+}
